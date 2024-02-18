0ce6ef4ff64f234dbefe93e85bcdde9a47ba1386
Indresh <indreshmr12345@gmail.com>
2024-02-18 22:53:39 +0530

    asyn fn in master branch

--- js_notes/cohertnotes/week1/10. async.js
+++ js_notes/cohertnotes/week1/10. async.js
@@ -1,7 +1,7 @@
 // Synchronous and asyncronous functions in javascript 
 
 //What does syncronous mean = together , one-after-another , one thing at a time
-//What does asyncronous mean = opp to syncronous , hapens in parts , multiple thins are context switching ith each other.
+//What does asyncronous mean = opp to syncronous , hapens in parts , multiple things are context switching with each other.
 
 //Can js conetxt switch - yes , using async functions. 
 
@@ -14,7 +14,7 @@ function findSum(n){
     console.log(sum);
 }
 findSum(100);
-//When we run this function the js thread is busy running it . 1st when the program is executed the control reaches line 9 and the jups to line 16 . After line 16 contol goes back to line 10 and then the for loops is being processed n times making the threads busy doing these calculations and not free . This is a syncronous function 
+//When we run this function the js thread is busy running it . 1st when the program is executed the control reaches line 9 and the jumps to line 16 . After line 16 contol goes back to line 10 and then the for loops is being processed n times making the threads busy doing these calculations and not free . This is a syncronous function 
 
 //async function example
 function sumOfn(n){
@@ -33,11 +33,11 @@ console.log("Hello");
 //How this is a async function - 
 //First when the program is executed the control reaches line 20 , the function sumOfn is declareed and the control moves to line 27 and the function findsumto10 is also declared. then control reaches line 30 where the global function settimeout is called and it has been asked to execute the findsumto10 function but only after 1 sec so instead of waiting there for a second and executing that line and then moving to the next function , the control reaches the next line and executes the 31st line and logs hello in the console and by that time 1 sec is completed and so the settimeout function also executes 
 
-//In this example we observe that instead of waiting in line 30 for a second and executing it , context switching happens and the control reaches next line and executes it , by that time 1sec happens and then the settimeout function is exected . 
+//In this example we observe that instead of waiting in line 30 for a second and executing it , context switching happens and the control reaches next line and executes it , by that time 1sec happens and then the settimeout function is processed and is waiting in the callback queue. When the  . 
 
 //Concept of busy wait 
 //We can get the same functionnality of delayed execution like the settimeout function but in syncronous nature using the busy wait concept
-//Here suppose we want a statement to be executed after few seconds of delay but syncronously then we use busy wait where we make the thread busy executing a function that is very expensive (time consuming) and that creates a delay in the execution of the next statement 
+//Here suppose we want a statement to be executed after few seconds of delay but syncronously then we use busy wait where we make the thread busy executing a function that is very expensive (time consuming) and that creates a delay in the execution of the next statement
 
 
 function syncSleep(){
@@ -79,4 +79,18 @@ for(let i=0;i<=100;i++){
     sum += i;
 }
 console.log(sum);
+
+
+
+// 1. Before promise we used to depend on callback functions which would result in 1.) Callback Hell (Pyramid of doom) | 2.) Inversion of control
+// 2. Inversion of control is overcome by using promise.
+//   2.1) A promise is an object that represents eventual completion/failure of an asynchronous operation.
+//   2.2) A promise has 3 states: pending | fulfilled | rejected.
+//   2.3)  As soon as promise is fulfilled/rejected => It updates the empty object which is assigned undefined in pending state.
+//   2.4) A promise resolves only once and it is immutable. 
+//   2.5) Using .then() we can control when we call the cb(callback) function.
+
+// 3. To avoid callback hell (Pyramid of doom) => We use promise chaining. This way our code expands vertically instead of horizontally. Chaining is done using '.then()'
+// 4. A very common mistake that developers do is not returning a value during chaining of promises. Always remember to return a value. This returned value will be used by the next .then()
+
 
