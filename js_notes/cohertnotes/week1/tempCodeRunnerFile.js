b567d921d369f585a0c6e03b7bcfea03fcd9a84f
Indresh <indreshmr12345@gmail.com>
2024-02-24 22:01:26 +0530

    changed file structure

--- js_notes/cohertnotes/week1/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-let getamt = indresh.withdraw(30000);
-// console.log(getamt);
-
-// let getbal = indresh.namebal(32434523);
-// console.log(getbal);
-
--- js_notes/js_hithesh.js/lec3.js
+++
@@ -1,4 +0,0 @@
-function shout(str){
-    return console.log(str + str);
-}
-shout('Fire');
